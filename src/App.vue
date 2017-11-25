929e4103596d15a369502bbabe44ff35891ae764
Paavo Väänänen <vaananen.paavo@gmail.com>
2017-11-25 23:51:13 +0200

    fixed mozilla css pulse color

--- src/App.vue
+++ src/App.vue
@@ -80,7 +80,7 @@
     -webkit-box-shadow: 0 0 0 0 rgba(124,252,0, 0.4);
   }
   70% {
-      -webkit-box-shadow: 0 0 0 20px rgba(124,252,0, 0);
+      -webkit-box-shadow: 0 0 0 50px rgba(124,252,0, 0);
   }
   100% {
       -webkit-box-shadow: 0 0 0 0 rgba(124,252,0, 0);
@@ -89,16 +89,16 @@
 
   @keyframes pulse {
   0% {
-    -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
-    box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+    -moz-box-shadow: 0 0 0 0 rgba(124,252,0, 0.4);
+    box-shadow: 0 0 0 0 rgba(124,252,0, 0.4);
   }
   70% {
-      -moz-box-shadow: 0 0 0 10px rgba(204,169,44, 0);
-      box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+      -moz-box-shadow: 0 0 0 10px rgba(124,252,0, 0.4);
+      box-shadow: 0 0 0 10px rgba(124,252,0, 0.4);
   }
   100% {
-      -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0);
-      box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+      -moz-box-shadow: 0 0 0 0 rgba(124,252,0, 0.4);
+      box-shadow: 0 0 0 0 rgba(124,252,0, 0.4);
   }
 }
 
