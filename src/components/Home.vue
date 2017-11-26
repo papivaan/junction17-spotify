ee1ccbde0003001de89e0d45b4031999e60d647b
Paavo Väänänen <vaananen.paavo@gmail.com>
2017-11-26 10:47:08 +0200

    Added description to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,29 @@
 <template>
+  <div>
   <div class="playlist">
     <h1>{{ msg }}</h1>
+  </div>
+    <div class="description">
+      <p>Welcome to use our Spotify API powered party music list!</p>
+      <p>
+        On <b>Request</b> tab you can search songs
+        by song name and then ask for the DJ to confirm
+        them to be added to the playlist. Or simply search for samba.
+        We recommend samba!
+      </p>
+      <p>
+        <b>DJ</b> tab is for the DJ to see all the requested songs
+        and either <b>confirm</b> or <b>decline</b> them.
+      </p>
+      <p>
+        The <b>Playlist</b> tab is where you can see all the songs added
+        to the playlist.
+      </p>
+    </div>
+    <div class="tech">
+      <p>This project was built using <b>Vue.js</b>, <b>Node.js</b>,
+      <b>Firebase</b> and <b>Heroku</b>.</p>
+    </div>
   </div>
 </template>
 
